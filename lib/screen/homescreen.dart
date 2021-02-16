995eedcc92ac1c5c2a03a3d2d1856becb92314bc
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-02-16 13:53:13 +0600

    custompaint working is file

--- lib/screen/homescreen.dart
+++ lib/screen/homescreen.dart
@@ -1,11 +1,17 @@
+import 'package:demo_player/widgets/tabbar_curve.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Demo Player"),
+      body: Container(
+        child: TopBar(),
       ),
     );
   }
