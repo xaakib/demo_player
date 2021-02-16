b799546618e2fd3b10fdbb35e1389020544038f3
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-02-16 15:12:33 +0600

    csutomDrawer is done

--- lib/screen/memorize_scree.dart
+++ lib/screen/memorize_scree.dart
@@ -6,10 +6,41 @@ class MemorizeScreen extends StatefulWidget {
 }
 
 class _MemorizeScreenState extends State<MemorizeScreen> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      key: _scaffoldKey,
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            Container(
+              height: 200,
+              width: MediaQuery.of(context).size.width,
+              color: Colors.red,
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(
+            Icons.menu,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            _scaffoldKey.currentState.openDrawer();
+          },
+        ),
+        title: Text(
+          "Memorize",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+      ),
     );
   }
 }
